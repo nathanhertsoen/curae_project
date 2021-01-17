d1e0b56ed60d9c586c1c2f1bfcc961b203c6a894
Nathan <hertsoen.n@hotmail.fr>
2021-01-17 13:38:05 +0100

    Fix script bug
    Bad initialization format

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,3 +9,6 @@ INSERT INTO docteur VALUES (3, 'Johnson','McKalagan','H',2);
 INSERT INTO service VALUES (1, 'Chirurgie','Discipline médicale spécialisée dans le traitement des maladies et des traumatismes, qui consiste à pratiquer, manuellement et à l''aide d''instruments, des actes opératoires sur un corps vivant.');
 INSERT INTO service VALUES (2, 'Urgence','Prise en charge continue des personnes blessées, malades ou victimes de phénomènes exceptionnels et difficilement prévisibles');
 INSERT INTO service VALUES (3, 'Radiologie','Spécialité médicale qui utilise des rayons X ou des ultrasons pour obtenir des images d''une partie du corps ou des organes (radiographie, échographie, scanner, IRM).');
+
+-- FORMAT CONVERSION ACTIVE : "yyyy-MM-dd+1" TO "dd-MM-yyyy"
+INSERT INTO hospitalization VALUES (1, 1, 1, 1, '1970-01-02', '2021-01-22');
