8089527a007d4c144ba23cfbb90d27f22dbd5c0e
WassimDjamaa <56625264+WassimDjamaa@users.noreply.github.com>
2021-01-17 13:03:22 +0100

    test data into table doctor and add commentary

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,8 @@
 INSERT INTO patient VALUES (1, 'Wassim','DJAMAA','H');
 INSERT INTO patient VALUES (2, 'Nathan','HERTSOEN','H');
 INSERT INTO patient VALUES (3, 'Cyril','PERARD','H');
+
+
+INSERT INTO docteur VALUES (1, 'Stéphane','LEROY','H','1');
+INSERT INTO docteur VALUES (2, 'Michèle','LECUYER','H','2');
+INSERT INTO docteur VALUES (3, 'Suzane','PERARD','F','2');
