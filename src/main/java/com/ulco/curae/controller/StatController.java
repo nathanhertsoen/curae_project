3585c91ef86173f0daa01d62d5fd951a8a44b018
WassimDjamaa <56625264+WassimDjamaa@users.noreply.github.com>
2021-01-18 20:14:47 +0100

    test
    test d'un commit

---
+++ src/main/java/com/ulco/curae/controller/StatController.java
@@ -0,0 +1,24 @@
+package com.ulco.curae.controller;
+
+import com.ulco.curae.dto.StatDTO;
+import com.ulco.curae.service.IStatService;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/stats")
+public class StatController {
+    @ApiModelProperty("Powered by DHP Technology ©")
+
+    @Autowired
+    private IStatService statService;
+
+    @GetMapping
+    @ApiOperation("Selectionner toutes les stats.")
+    public List<StatDTO> getAll() {
+        return statService.getAll();
+    }
+}
